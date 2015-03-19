775c4441f4aee1fcd75ac78d7e270cb404e81d49
BrightsCode <zhangchao@Bright.local>
2015-03-20 00:21:06 +0800

    Add SVProgressHUD

--- DoubanMovie/CommonMacro/DM_API_Macro.h
+++ DoubanMovie/CommonMacro/DM_API_Macro.h
@@ -14,37 +14,6 @@
  *
  *  @since 1.0
  */
-#define API_Base_URL @"http://api.douban.com/"
-
-#pragma mark 榜单
-/**
- *  正在上映
- *
- *  @since 1.0
- */
-#define API_MovieList_NowPlaying @"v2/movie/nowplaying"
-
-/**
- *  top 250
- *
- *  @since 1.0
- */
-#define API_MovieList_Top250 @"/v2/movie/top250"
-
-/**
- *  口碑榜
- *
- *  @since 1.0
- */
-#define API_MovieList_Weekly @"/v2/movie/weekly"
-
-#pragma mark 用户
-/**
- *  喜欢
- *
- *  @since 1.0
- */
-#define API_MovieList_Like @"/v2/movie/weekly"
-
+#define API_Base_URL @"http://api.douban.com/v2/movie"
 
 #endif
--- Podfile
+++ Podfile
@@ -4,3 +4,4 @@ pod "AFNetworking","~>2.5.1"
 pod "SDWebImage","~>3.6"
 pod "HMSegmentedControl","~>1.5"
 pod "MJRefresh"
+pod "SVProgressHUD",:head
